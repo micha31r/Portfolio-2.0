43fe3bd3780f730111b163e96af18a72b0f450e6
Michael Ren <michaelren0129@gmail.com>
2021-12-06 12:41:45 +1300

    Edited things

--- src/js/card.js
+++ src/js/card.js
@@ -1,10 +1,14 @@
-window.onload = function() {
-	document.querySelectorAll(".card").forEach(function(elem) {
-		elem.onclick = function() {
-			const link = elem.getAttribute("href");
-			if (link) {
-				window.open(link, '_blank').focus();
-			}
+document.querySelectorAll(".card").forEach(function(elem) {
+	elem.onclick = function() {
+		const link = elem.getAttribute("href");
+		if (link) {
+			window.open(link, '_blank').focus();
 		}
-	});
-}
+	}
+	elem.onmouseover = function() {
+		elem.querySelector(".title").style.opacity = 1;
+	}
+	elem.onmouseleave = function() {
+		elem.querySelector(".title").style.opacity = 0;
+	}
+});
---
+++ src/js/easterEgg.js
@@ -0,0 +1,17 @@
+const COLORS = [
+	"rgb(82,42,251)", 	// Blue
+	"rgb(209,156,0)", 	// Yellow
+	"rgb(224,45,72)", 	// Red
+	"rgb(158,45,224)", 	// Purple
+	"rgb(20,156,59)",	// Green
+]
+var index = 0;
+
+document.querySelector(".name span").onclick = function() {
+	index++;
+	if (index > COLORS.length-1) {
+		index = 0;
+	}
+	let color = COLORS[index]
+	document.documentElement.style.setProperty('--color-primary', color);
+}
