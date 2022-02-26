5f65c4c36241d45106c7d989b488e17a9141fd3c
Michael Ren <michaelren0129@gmail.com>
2022-02-26 21:25:19 +1300

    Updated styling

--- js/easterEgg.js
+++ js/easterEgg.js
@@ -1,9 +1,9 @@
 const COLORS = [
+	"rgb(20,156,59)",	// Green
 	"rgb(82,42,251)", 	// Blue
 	"rgb(209,156,0)", 	// Yellow
 	"rgb(224,45,72)", 	// Red
 	"rgb(158,45,224)", 	// Purple
-	"rgb(20,156,59)",	// Green
 ]
 var index = 0;
 
